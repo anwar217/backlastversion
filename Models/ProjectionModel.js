0539b34c329eeaa0c04702bd3965ba61782ef593
AnwarBensouissi <anwar777bensouissi@gmail.com>
2023-05-24 17:44:50 +0100

    first commit

---
+++ Models/ProjectionModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+const { Schema } = mongoose;
+
+const ProjectionSchema = new Schema({
+    ProjectionId: { type: Number, required: true },
+    dateProjection: { type: Date, required: true },
+    prix: { type: Number, required: true },
+    cinemaId: { type: Schema.Types.ObjectId, ref: 'Cinema', required: true },
+    filmId: { type: Schema.Types.ObjectId, ref: 'Movie', required: true },
+    //tickets: [{ type: Schema.Types.ObjectId, ref: 'Ticket' }],
+    //seance: { type: Schema.Types.ObjectId, ref: 'Seance', required: true }
+});
+
+const Projection = mongoose.model('Projection', ProjectionSchema);
+export default Projection;
